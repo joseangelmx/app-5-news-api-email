68d066c2f4762f39c99c24a97c4ff41fd3131711
Jose Angel Vazquez Torres <joseangelvazqueztorresmx1231@gmail.com>
2024-07-26 14:10:11 -0600

    Day 28: ADD LINKS SPECIFY, LANGUAGE, LIMIT NEWS, ADD SUBJECT, CONTROL CATEGORY

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from send_mail import send_email
+topic = "bitcoin"
 api_key = "dcafa7e13c1a46f88178a3c6bd4ac2d9"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-    "sortBy=publishedAt&apiKey="\
-    "dcafa7e13c1a46f88178a3c6bd4ac2d9"
+url = f"https://newsapi.org/v2/everything?"\
+      f"q={topic}&"\
+      "sortBy=publishedAt&"\
+      "apiKey=dcafa7e13c1a46f88178a3c6bd4ac2d9&" \
+      "language=es"
 
 # Make request
 request = requests.get(url)
@@ -13,8 +16,12 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Today's news" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
+
 body = body.encode("utf-8")
-send_email(body)
+send_email(message=body)
